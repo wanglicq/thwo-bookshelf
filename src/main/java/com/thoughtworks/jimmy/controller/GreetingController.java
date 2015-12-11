d8a37116f14e3ac75835c27d409f840eebea8742
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-11 23:59:22 +0800

    [#0] make more meaningful sense for demo.

--- src/main/java/com/thoughtworks/jimmy/controller/GreetingController.java
+++ src/main/java/com/thoughtworks/jimmy/controller/GreetingController.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.jimmy.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +12,12 @@ public class GreetingController {
     @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public ModelAndView greeting(@PathVariable String name) {
 
-        ModelMap model = new ModelMap();
-        model.put("name", name);
-        return new ModelAndView("hello", model);
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("hello");
+        modelAndView.addObject("name", name);
+
+        return modelAndView;
 
     }
 

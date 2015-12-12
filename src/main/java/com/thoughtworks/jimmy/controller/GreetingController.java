7b9bfc1009414bdb966bcfa09d7c4ea5bf893a52
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-12 12:39:46 +0800

    [#0] add Person model in mini demo.

--- src/main/java/com/thoughtworks/jimmy/controller/GreetingController.java
+++ src/main/java/com/thoughtworks/jimmy/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.jimmy.controller;
 
+import com.thoughtworks.jimmy.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,8 @@ public class GreetingController {
 
         ModelAndView modelAndView = new ModelAndView();
 
+        modelAndView.addObject("person",  new Person(name));
         modelAndView.setViewName("hello");
-        modelAndView.addObject("name",  name.toUpperCase());
 
         return modelAndView;
 

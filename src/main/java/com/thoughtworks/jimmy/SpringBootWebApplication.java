c340522b04d243e1b65301833d1e6def74dcced4
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-12 17:18:59 +0800

    [#0] print applicationContext when springboot start.

--- src/main/java/com/thoughtworks/jimmy/SpringBootWebApplication.java
+++ src/main/java/com/thoughtworks/jimmy/SpringBootWebApplication.java
@@ -2,11 +2,22 @@ package com.thoughtworks.jimmy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootWebApplication.class, args);
+        String[] names = applicationContext.getBeanDefinitionNames();
+
+        Arrays.sort(names);
+
+        System.out.println("applicationContext via Spring Boot:");
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
